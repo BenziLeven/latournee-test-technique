82a0649b592417a9e682a5fb8d615093f1604f4a
BenziLeven <benzakeinjeremy@gmail.com>
2023-10-24 16:30:03 +0200

    feat: fetch landing page data

--- latournee-test-technique/src/pages/IndexPage.vue
+++ latournee-test-technique/src/pages/IndexPage.vue
@@ -1,49 +1,36 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
-  </q-page>
+    <q-page class="row items-center justify-evenly">
+        <div v-if="subcategories.length > 0">
+            <div v-for="subcategory in subcategories" v-bind:key="subcategory.ID">
+                hello
+            </div>
+        </div>
+        <div v-else>
+            Loading
+        </div>
+    </q-page>
 </template>
 
 <script lang="ts">
-import type { Todo, Meta } from "components/models";
-import ExampleComponent from "components/ExampleComponent.vue";
 import { defineComponent, ref } from "vue";
+import { getLanding } from "../services";
+
+import type {SubCategory} from "../components/models"
 
 export default defineComponent({
-    name: "IndexPage",
-    components: { ExampleComponent },
-    setup() {
-        const todos = ref<Todo[]>([
-            {
-                id: 1,
-                content: "ct1"
-            },
-            {
-                id: 2,
-                content: "ct2"
-            },
-            {
-                id: 3,
-                content: "ct3"
-            },
-            {
-                id: 4,
-                content: "ct4"
-            },
-            {
-                id: 5,
-                content: "ct5"
-            }
-        ]);
-        const meta = ref<Meta>({
-            totalCount: 1200
-        });
-        return { todos, meta };
+    data() {
+        return {
+            subcategories: null as SubCategory
+        };
+    },
+    async beforeRouteEnter(to, from, next) {
+        const data = await getLanding();
+        next(vm => vm.setData(data));
+    },
+    methods: {
+        setData(pageContent) {
+            this.subcategories = pageContent["Sous-categories"];
+        }
     }
 });
 </script>
